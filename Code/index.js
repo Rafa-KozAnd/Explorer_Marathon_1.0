026faf1f024da469267afe6c9e92c0ee8cf88cf4
Rafael Kozlowski Andreola <rafandreola@hotmail.com>
2022-06-12 18:56:53 -0300

    Add files via upload

---
+++ Code/index.js
@@ -0,0 +1,48 @@
+const elementoResposta = document.querySelector("#resposta")
+const inputPergunta = document.querySelector("#inputPergunta")
+const buttonPerguntar = document.querySelector("#buttonPerguntar")
+const respostas = [
+  "Certeza!",
+  "Não tenho tanta certeza.",
+  "É decididamente assim.",
+  "Não conte com isso.",
+  "Sem dúvidas!",
+  "Pergunte novamente mais tarde.",
+  "Sim, definitivamente!",
+  "Minha resposta é não.",
+  "Você pode contar com isso.",
+  "Melhor não te dizer agora.",
+  "A meu ver, sim.",
+  "Minhas fontes dizem não.",
+  "Provavelmente.",
+  "Não é possível prever agora.",
+  "Perspectiva boa.",
+  "As perspectivas não são tão boas.",
+  "Sim.",
+  "Concentre-se e pergunte novamente.",
+  "Sinais apontam que sim.",
+]
+
+function fazerPergunta() {
+
+  if(inputPergunta.value == "") {
+    alert("Digite sua pergunta")
+    return
+  }
+
+  buttonPerguntar.setAttribute("disabled", true)
+
+  const pergunta = "<div>" + inputPergunta.value + "</div>"
+
+  const totalRespostas = respostas.length
+  const numeroAleatorio = Math.floor(Math.random() * totalRespostas)
+
+  elementoResposta.innerHTML = pergunta + respostas[numeroAleatorio]
+
+  elementoResposta.style.opacity = 1;
+  // Sumir com a resposta depois de 3 seg
+  setTimeout(function() {
+    elementoResposta.style.opacity = 0;
+    buttonPerguntar.removeAttribute('disabled')
+  }, 3000) //3000 milissegundos
+}
